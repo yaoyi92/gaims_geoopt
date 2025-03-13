b6e1fb2ed368978ef9fc7a88f3c4d2029113d8a9
Yi Yao <yaoyi92@gmail.com>
2025-03-13 11:03:07 -0500

    first commit

---
+++ src/gaims_geoopt/flows.py
@@ -0,0 +1,80 @@
+from atomate2.ase.jobs import GFNxTBStaticMaker
+from jobflow import Flow, job, Response
+from autoplex.fitting.common.jobs import machine_learning_fit
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+@job 
+def check_convergence_and_next(struct, database_dict, max_force, max_force_criteria):
+    if max_force < max_force_criteria:
+        logging.info(
+            f"MLIP assisted Geometry Optimization Converged with max_force: {max_force} < {max_force_criteria}"
+        )
+
+        return None
+    logging.info(
+        f"MLIP assisted Geometry Optimization continues with max_force: {max_force} > {max_force_criteria}"
+    )
+    job_macefit = machine_learning_fit(
+                      database_dir=None,
+                      database_dict=database_dict,
+                      run_fits_on_different_cluster=True,
+                      name="MACE",
+                      mlip_type="MACE",
+                      ref_energy_name="REF_energy",
+                      ref_force_name="REF_forces",
+                      ref_virial_name=None,
+                      species_list=None,
+                      num_processes_fit=1,
+                      foundation_model="small",
+                      multiheads_finetuning=False,
+                      loss="ef",
+                      energy_weight = 1.0,
+                      forces_weight = 1.0,
+                      stress_weight = 0.0,
+                      E0s = "isolated",
+                      scaling = "rms_forces_scaling",
+                      batch_size = 2,
+                      max_num_epochs = 1000,
+                      ema=True,
+                      ema_decay = 0.99,
+                      amsgrad=True,
+                      default_dtype = "float64",
+                      keep_isolated_atoms=True,
+                      lr = 0.01,
+                      patience = 500,
+                      device = "cpu",
+                      save_cpu =True,
+                      seed = 3,
+                  )
+    job_relax = get_mace_relax_job(job_macefit.output, struct)
+    job_static = GFNxTBStaticMaker(
+        calculator_kwargs={"method": "GFN2-xTB"},
+    ).make(job_relax.output.output.molecule)
+    job_max_force = evaluate_max_force(job_static.output.output.forces)
+    job_add_database = add_structure_database(database_dict, job_static.output.output.mol_or_struct)
+    job_check_convergence_and_next = check_convergence_and_next(job_static.output.output.mol_or_struct,
+                                                                job_add_database.output,
+                                                                job_max_force.output,
+                                                                max_force_criteria
+                                                                )
+    flow = Flow([job_macefit, job_relax, job_static, job_max_force, job_add_database, job_check_convergence_and_next])
+    return Response(replace=flow)
+
+    
+def define_flow(molecule, database_dict, max_force_criteria):
+    job_static = GFNxTBStaticMaker(
+        calculator_kwargs={"method": "GFN2-xTB"},
+    ).make(molecule)
+    job_max_force = evaluate_max_force(job_static.output.output.forces)
+    job_add_database = add_structure_database(database_dict, job_static.output.output.mol_or_struct)
+    job_check_convergence_and_next = check_convergence_and_next(job_static.output.output.mol_or_struct,
+                                                                job_add_database.output,
+                                                                job_max_force.output,
+                                                                max_force_criteria
+                                                                )
+    jobs = [job_static, job_max_force, job_add_database, job_check_convergence_and_next]
+    return Flow(jobs)
---
+++ src/gaims_geoopt/jobs.py
@@ -0,0 +1,29 @@
+from atomate2.forcefields.jobs import ForceFieldRelaxMaker
+from atomate2.forcefields import MLFF
+from jobflow import Flow, job, Response
+
+@job
+def evaluate_max_force(forces):
+    forces = np.array(forces)
+    return np.max(np.sum(forces**2, axis=1)**0.5)
+
+@job
+def add_structure_database(database_dict, mol_or_struct):
+    mol_or_struct_copy = mol_or_struct.copy()
+    mol_or_struct_copy.properties["REF_energy"] = mol_or_struct.properties["energy"]
+    mol_or_struct_copy.properties["REF_virial"] = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+    for i in range(len(mol_or_struct)):
+        mol_or_struct_copy.sites[i].properties["REF_forces"] = mol_or_struct.properties["forces"][i]
+    database_dict["train.extxyz"].append(mol_or_struct_copy)
+    database_dict["test.extxyz"].append(mol_or_struct_copy)
+    return database_dict
+
+@job
+def get_mace_relax_job(mlip_output, struct):
+    mace_maker = ForceFieldRelaxMaker(
+        force_field_name = MLFF.MACE,
+        calculator_kwargs = {'model':f'{mlip_output["mlip_path"][0]}/MACE_compiled.model'},
+        relax_kwargs = {'fmax':0.025})
+    job_relax = mace_maker.make(struct)
+    flow = Flow([job_relax,])
+    return Response(replace=flow, output=job_relax.output)
