bfd5c99c2f9d12a8e62900c3ff8bb5f4120058ea
Yi Yao <yaoyi92@gmail.com>
2025-04-01 10:11:52 -0500

    add option to change max_steps for mlip relaxer

--- src/gaims_geoopt/jobs.py
+++ src/gaims_geoopt/jobs.py
@@ -21,10 +21,15 @@ def add_structure_database(database_dict, mol_or_struct, forces):
 
 @job
 def get_mace_relax_job(mlip_output, struct, max_force_criteria, relax_calculator_kwargs):
+    steps = 500
+    if "max_steps" in relax_calculator_kwargs:
+        steps = relax_calculator_kwargs["max_steps"]
+        del relax_calculator_kwargs["max_steps"]
     calculator_kwargs = {'model':f'{mlip_output["mlip_path"][0]}/MACE_compiled.model'}
     calculator_kwargs.update(relax_calculator_kwargs)
     mace_maker = ForceFieldRelaxMaker(
         force_field_name = MLFF.MACE,
+        steps=steps,
         calculator_kwargs = calculator_kwargs,
         relax_kwargs = {'fmax':max_force_criteria/10})
     job_relax = mace_maker.make(struct)
